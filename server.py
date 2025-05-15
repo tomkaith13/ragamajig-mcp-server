7502e781685fee2c75a49b8b32c9e35a4ceee302
Bibin Thomas <bthomas@league.com>
2025-05-15 09:06:52 -0400

    added commit

---
+++ server.py
@@ -0,0 +1,31 @@
+# server.py
+from mcp.server.fastmcp import FastMCP
+import requests
+import json
+# Create an MCP server
+mcp = FastMCP("Demo")
+
+
+# Add an addition tool
+@mcp.tool()
+def add(a: int, b: int) -> int:
+    """Add two numbers"""
+    print("bibin was here!!!")
+    return a + b
+
+@mcp.tool()
+def search_answer(question_about_capability: str) -> str:
+    """Search for an answer to any question from the user related to Documents capability"""
+    # Here you can implement your logic to answer the query
+    # For demonstration, we'll just return a simple response
+    url = 'http://localhost:8000/ask_question'
+    headers = {'Content-Type': 'application/json'}
+    # data = {'query': 'Where is the config files located?'}
+    data = {
+        "query": f"{question_about_capability}",
+    }
+    response = requests.post(url, data=data,headers=headers)
+    # mcp.logger.info(f"Response from the server: {response}")
+    return response.json()
+
+mcp.run()
